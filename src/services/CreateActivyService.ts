0f9b51e249ebbc8431269205cb436a903862cf71
Vanessa-Mayumi <82035304+Vanessa-Mayumi@users.noreply.github.com>
2021-10-10 09:56:56 -0300

    s3
    s3

---
+++ src/services/CreateActivyService.ts
@@ -0,0 +1,23 @@
+interface ActivyData{
+    name: string;
+    activy_date: string;
+    grade: number;
+    courseUnitId: string;
+}
+
+class CreateActivyService{
+    public async execute(data:ActivyData){
+        const {name,activy_date,grade,courseUnitId} = data;
+
+        const activy = {
+            name,
+            activy_date,
+            grade,
+            courseUnitId
+        }
+
+        return activy;
+    }
+}
+
+export {CreateActivyService};
